67c977f5395d34f442a3c6fb016a98ae92aa7bbc
viniciusmorao-dev <storagemorao@gmail.com>
2026-03-22 17:00:24 -0300

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -39,11 +39,18 @@ def call(Map config) {
                 steps {
                     script {
                         sh """
-                        rm -rf infra || true
+                        rm -rf infra
                         git clone https://github.com/viniciusmorao-dev/infra-demo-app.git infra
                         cd infra
                         sed -i 's/APP1_VERSION=.*/APP1_VERSION=${TAG}/' .env
-                        docker-compose down
+
+                        rm -rf nginx/default.conf || true
+
+                        if [ ! -f nginx/default.conf ]; then
+                            echo "ERRO: arquivo nginx/default.conf não encontrado!"
+                            exit 1
+                        fi
+                        
                         docker-compose up -d --build
                         """
                     }
