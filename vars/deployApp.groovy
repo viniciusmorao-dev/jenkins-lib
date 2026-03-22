5e7575b5f29a325441923121a78e99900e7f63dc
viniciusmorao-dev <storagemorao@gmail.com>
2026-03-22 16:07:40 -0300

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -42,7 +42,6 @@ def call(Map config) {
                         rm -rf infra || true
                         git clone https://github.com/viniciusmorao-dev/infra-demo-app.git infra
                         cd infra
-                        ls -l nginx/default.conf
                         sed -i 's/APP1_VERSION=.*/APP1_VERSION=${TAG}/' .env
                         docker-compose down || true
 
